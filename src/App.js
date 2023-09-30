e9057f876d0ac47315d8ed75ad7248d75c5b62df
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-09-30 10:13:29 +0700

    styling: body and navbar style css

--- src/App.js
+++ src/App.js
@@ -14,37 +14,42 @@ const App = () => {
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
-        <div className="movie-wrapper" key={i}>
-          <div className="movie-title">{movie.title}</div>
+        <section className="movie-wrapper" key={i}>
           <img
             className="movie-image"
             src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
           />
-          <div className="movie-date">{movie.release_date}</div>
-          <div className="movie-rate">{movie.vote_average}</div>
-        </div>
+          <div className="movie-title">{movie.title}</div>
+          <section className="movie-describe">
+            <div className="movie-rate">{movie.vote_average}</div>
+            <div className="movie-date">{movie.release_date}</div>
+            <div className="category">Movie</div>
+          </section>
+        </section>
       );
     });
   };
 
-  const search = async(q) => {
+  const search = async (q) => {
     const query = await searchMovie(q);
-    setPopularMovies(query.results)
+    setPopularMovies(query.results);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Pandhu Movie</h2>
+      <navbar className="navbar">
+        <h2>
+          PandhuMovie<span style={{ color: "blue" }}>.</span>
+        </h2>
         <input
           placeholder="Search..."
           className="movie-search"
           onChange={({ target }) => search(target.value)}
         />
-        <div className="movie-container">
-          <PopularMovieList />
-        </div>
-      </header>
+      </navbar>
+      <div className="movie-container">
+        <PopularMovieList />
+      </div>
     </div>
   );
 };
