8ef8082a95b9fbea84283a768778f7a5e22c9359
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-28 04:27:30 +0700

    [chore] added now playing api, use tryca

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,38 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiKey = process.env.REACT_APP_APIKEY
-const baseUrl = process.env.REACT_APP_BASEURL
+const apiKey = process.env.REACT_APP_APIKEY;
+const baseUrl = process.env.REACT_APP_BASEURL;
 
-export const getMovieList = async() => {
-    const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
-    return movie.data.results
-}
+export const popularMovie = async () => {
+  try {
+    const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
+    return movie.data.results;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Gagal melakukan pencarian film");
+  }
+};
 
 export const searchMovie = async (q) => {
-    const search = await axios.get(`${baseUrl}/search/movie?query=${q}&api_key=${apiKey}`)
-    return search.data
-}
+  try {
+    const search = await axios.get(
+      `${baseUrl}/search/movie?query=${q}&api_key=${apiKey}`
+    );
+    return search.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Gagal melakukan pencarian film");
+  }
+};
+
+export const nowPlaying = async () => {
+  try {
+    const moviePlaying = await axios.get(
+      `${baseUrl}/movie/now_playing?language=en-US&page=1&api_key=${apiKey}`
+    );
+    return moviePlaying.data.results; 
+  } catch (error) {
+    console.error(error);
+    throw new Error("Gagal mendapatkan film yang sedang diputar");
+  }
+};
