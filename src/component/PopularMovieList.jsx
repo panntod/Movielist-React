dc612e215b9c782bfb1dc898ea609fd12c864d65
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-28 04:29:01 +0700

    [doc] added title

--- src/component/PopularMovieList.jsx
+++ src/component/PopularMovieList.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const PopularMovieList = ({ popularMovies, isLoading }) => {
-  console.log(popularMovies)
   return (
     <>
       {isLoading ? (
@@ -50,27 +49,30 @@ const PopularMovieList = ({ popularMovies, isLoading }) => {
           </svg>
         </div>
       ) : (
-        <div className="movie-container">
-          {popularMovies.map(
-            (movie, i) =>
-              // Periksa apakah vote_average tidak sama dengan 0
-              movie.vote_average !== 0 && (
-                <section className="movie-wrapper" key={i}>
-                  <img
-                    className="movie-image"
-                    src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <div className="movie-title">{movie.title}</div>
-                  <section className="movie-describe">
-                    <div className="movie-rate">{movie.vote_average}</div>
-                    <div className="movie-date">{movie.release_date}</div>
-                    <div className="category">Movie</div>
+        <>
+          <h1 className="title">Most Popular Movie</h1>
+          <div className="movie-container">
+            {popularMovies.map(
+              (movie, i) =>
+                // Periksa apakah vote_average tidak sama dengan 0
+                movie.vote_average !== 0 && (
+                  <section className="movie-wrapper" key={i}>
+                    <img
+                      className="movie-image"
+                      src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+                      alt={movie.title}
+                    />
+                    <div className="movie-title">{movie.title}</div>
+                    <section className="movie-describe">
+                      <div className="movie-rate">{movie.vote_average}</div>
+                      <div className="movie-date">{movie.release_date}</div>
+                      <div className="category">Movie</div>
+                    </section>
                   </section>
-                </section>
-              )
-          )}
-        </div>
+                )
+            )}
+          </div>
+        </>
       )}
     </>
   );
